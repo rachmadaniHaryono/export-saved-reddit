5efcbec01dd3c4545058e8222360eaaeb2ea3a29
rachmadani haryono <foreturiga@gmail.com>
2015-08-07 09:46:50 +0800

    replace comma in title

--- export-saved.py
+++ export-saved.py
@@ -61,7 +61,7 @@ def main():
     for i in r.user.get_saved(limit=None, time='all'):
         if not hasattr(i, 'title'):
            i.title = i.link_title
-        export_csv += ("%s,%s,,%s\n" % (i.permalink.encode('utf-8'), i.title.encode('utf-8'), str(i.subreddit)))
+        export_csv += ("%s,%s,,%s\n" % (i.permalink.encode('utf-8'), i.title.encode('utf-8').replace(',',''), str(i.subreddit)))
     with open("export-saved.csv", "w") as f:
         f.write(export_csv)
     converter = Converter("export-saved.csv")
